5fd837dc406a5c11a26bce4ca9ed254a52be6960
Anshika bhatt <119714902+anshikabhatt@users.noreply.github.com>
2023-03-20 06:46:12 +0530

    final test

--- test.py
+++ test.py
@@ -1,44 +1,61 @@
 import os
-import csv
 import yaml
-import unittest
+import csv
 
-def test_supported_pairs_csv_export():
+def test_supported_pairs_csv():
+    # Run the code to generate the CSV file
     supported_pairs = []
-
-    # Loop through all the YAML files in the config directory
     ignore_files = ['MWPageLoader.yaml', 'languages.yaml', 'JsonDict.yaml', 'Dictd.yaml', 'mt-defaults.wikimedia.yaml']
     for filename in os.listdir("config"):
         if filename.endswith('.yaml') and filename not in ignore_files:
             with open(os.path.join("config", filename), "r") as f:
                 config = yaml.safe_load(f)
-                f.close
 
-                # Check if the YAML file has a "handler" key
                 if "handler" in config:
                     handler = config["handler"]
                     if handler == "transform.js":
                         lst = config["languages"]
-                        source_lang = filename[:-5]
-                        for target_lang in lst:
-                            supported_pairs.append({
-                                "source language": source_lang,
-                                "target language": target_lang,
-                                "translation engine": source_lang,
-                                "is preferred engine?": config.get("preferred_engine", False)
-                            })
+                        target_langs = []
+                        source_lang = []
+                        for j in lst:
+                            source_lang.append(j)
+                            target_lang = []
+                            for k in lst:
+                                if(j != k and not ((j == "simple" and k == "en") or (k == "simple" and j == "en"))):
+                                    target_lang.append(k)
+                            target_langs.append(target_lang)
+                        engine = filename[:-5]
+                        preferred_engine = config.get("preferred_engine", False)
                 else:
-                    # Handle standard configuration files
-                    for source_lang, target_langs in config.items():
-                        for target_lang in target_langs:
-                            supported_pairs.append({
-                                "source language": source_lang,
-                                "target language": target_lang,
-                                "translation engine": filename[:-5],
-                                "is preferred engine?": config.get("preferred_engine", False)
-                            })
+                    source_lang = list(config.keys())
+                    target_langs = []
+                    for j in source_lang:
+                        target_langs.append(config[j])
+                        engine = filename[:-5]
+                        preferred_engine = config.get("preferred_engine", False)
 
-    # Group the supported pairs by source language
+                for k in range(len(source_lang)):
+                    for i in target_langs[k]:
+                        supported_pairs.append({
+                            "source language": source_lang[k],
+                            "target language": i,
+                            "translation engine": engine,
+                            "is preferred engine?": preferred_engine
+                        })
+
+    with open("mt-defaults.csv", "r") as f:
+        reader = csv.DictReader(f)
+        mt_defaults = {}
+        for row in reader:
+            key = (row["source language"], row["target language"], row["translation engine"])
+            mt_defaults[key] = row
+
+    for pair in supported_pairs:
+        key = (pair["source language"], pair["target language"], pair["translation engine"])
+        match = mt_defaults.get(key)
+        if match:
+            pair["is preferred engine?"] = "True"
+
     source_to_targets = {}
     for pair in supported_pairs:
         source = pair["source language"]
@@ -47,24 +64,16 @@ def test_supported_pairs_csv_export():
             source_to_targets[source] = []
         source_to_targets[source].append(target)
 
-    # Export the list as a CSV file
     with open("supported_pairs.csv", "w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=["source language", "target language", "translation engine", "is preferred engine?"])
         writer.writeheader()
-        for source, targets in source_to_targets.items():
-            for target in targets:
-                writer.writerow({
-                    "source language": source,
-                    "target language": target,
-                    "translation engine": source,
-                    "is preferred engine?": any(pair["is preferred engine?"] for pair in supported_pairs if pair["source language"] == source and pair["target language"] == target)
-                })
+        writer.writerows(supported_pairs)
 
-    # Read the CSV file and check if it has the correct format
-    with open("supported_pairs.csv", "r") as f:
+    # Check that the CSV file was created and contains expected data
+    assert os.path.isfile("supported_pairs.csv"), "CSV file was not created"
+    with open("supported_pairs.csv", "r", newline="") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            assert "source language" in row
-            assert "target language" in row
-            assert "translation engine" in row
-            assert "is preferred engine?" in row
+            assert row["source language"] in source_to_targets, f"Unsupported source language {row['source language']} found in CSV file"
+            assert row["target language"] in source_to_targets[row["source language"]], f"Unsupported target language {row['target language']} found in CSV file for source language {row['source language']}"
+
